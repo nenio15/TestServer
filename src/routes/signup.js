f58f972c4469276810269294ae7300b332fa7120
nenio15 <2857yoon@gmail.com>
2025-04-18 01:52:53 +0900

    [FEAT] router setting8

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -16,6 +16,7 @@ router.post('/signup', async (req, res) => {
     return res.status(400).json({ message: '필수 항목 누락' });
   }
 
+  try{
   const [exist] = await pool.query('SELECT * FROM User WHERE email = ?', [email])
   if (exist.length > 0) {
     return res.status(400).json({ message: '이미 등록된 이메일입니다' });
@@ -23,7 +24,7 @@ router.post('/signup', async (req, res) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
   const date = new Date(+new Date() + 3240 * 10000).toISOString().split("T")[0];
-  try {
+  
     const [result] = await pool.query(
       'INSERT INTO User (email, password, name, userType, isApproved, createdAt)'
       [email, hashedPassword, name, userType, true, date] //관리자 승인은 미구현.
