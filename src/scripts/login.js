78ab8d84b51dfc139a8f0a310ab247f16473e423
nenio15 <2857yoon@gmail.com>
2025-04-18 01:04:05 +0900

    [FEAT] 로그인 회원가입 구현

---
+++ src/scripts/login.js
@@ -0,0 +1,40 @@
+ import dotenv from 'dotenv';
+import express from 'express';
+import bcrypt from 'bcrypt';
+import pool from '../config/db.js';
+
+dotenv.config();
+const router = express.Router();
+
+import jwt from 'jsonwebtoken';
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const [users] = await pool.query('SELECT * FROM User WHERE email = ?', [email]);
+    if (users.length === 0) {
+      return res.status(401).json({ message: '이메일 또는 비밀번호가 잘못되었습니다.' });
+    }
+
+    const user = users[0];
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: '이메일 또는 비밀번호가 잘못되었습니다.' });
+    }
+
+    const token = jwt.sign({ userId: user.id, userType: user.userType }, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    });
+
+    res.status(200).json({ message: '로그인 성공', token });
+  } catch (error) { 
+    console.error(error);
+    res.status(500).json({ message: '서버 오류' });
+  }
+});
+
+//에러 페이지 로드 404
+router.post((req,res)=>{
+	res.status(404).send('권한이 없습니다');
+});
---
+++ src/scripts/signup.js
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import bcrypt from 'bcrypt';
+import { PrismaClient } from '@prisma/client';
+import pool from '../config/db.js';
+
+dotenv.config();
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+router.post('/owner/signup', async (req, res) => {
+  const { email, password, name, userType } = req.body;
+
+  if (!email || !password || !name || !userType) {
+    return res.status(400).json({ message: '필수 항목 누락' });
+  }
+
+  const [exist] = await prisma.pool.query('SELECT * FROM User WHERE email = ?', email)
+  if (exist.length > 0) {
+    return res.status(400).json({ message: '이미 등록된 이메일입니다' });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const date = new Date(+new Date() + 3240 * 10000).toISOString().split("T")[0];
+  try {
+    const [result] = await pool.query(
+      'INSERT INTO User (email, password, name, userType, isApproved, createdAt)'
+      [email, hashedPassword, name, userType, true, date] //관리자 승인은 미구현.
+    )
+
+    return res.status(200).json({ message: '성공', userId: result.userId });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: '서버 에러' });
+  }
+});
+
+//에러 페이지 로드 404
+router.get((req,res)=>{
+	res.status(404).send('권한이 없습니다');
+});
+
+export default router;
