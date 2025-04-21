90f3374a25be9aa7beca1fe91b1ff9c57ed62064
nenio15 <2857yoon@gmail.com>
2025-04-21 15:33:57 +0900

    3

--- src/app.js
+++ src/app.js
@@ -9,11 +9,19 @@ import ownerRoutes from './routes/owner.js';
 const app = express();
 app.use(express.json());
 
+//디버깅용
+app.use((req, res, next) => {
+  console.log(`요청 메서드: ${req.method}, 경로: ${req.path}`);
+  next();
+});
+
 // 라우트 예시
 app.get('/', (req, res) => {
   res.send('Hello, MongoDB Backend!');
 });
 app.use('/auth', authRoutes);
 app.use('/owner', ownerRoutes);
+
+
 
 export default app;
